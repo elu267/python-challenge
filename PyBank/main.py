0c7e700308f7f4be28b0ecc6774c179a2f459166
Erica <erica@ELU-MBPro.local>
2018-10-12 08:06:13 -0500

    completed updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,67 +2,68 @@
 import os
 import csv 
 
-# Path to collect data from the Resources folder
-# need to adjust this because the file is not 
-# sitting in the PyBank folder
-
-# getting the csv file (data set)
+# Getting the csv file (data set)
 csvFile = os.path.join('budget_data.csv')
 
-# creating a variable to store the data file
+# Creating a variable to store the data file
 csvDataset = []
-# creating a variable to store the dates
+# Creating a variable to store the dates
 csvDataset_Month = []
-# creating a variable to store the profits and losses
+# Creating a variable to store the profits and losses
 csvDataset_Amount = []
 
-# creating the line separators for the final summary print out
-# love this short cut, found it in "Learn Python the Hard Way"
+# Creating the line separators for the final summary print out
+# Love this short cut, found it in "Learn Python the Hard Way"
 lines = "-" * 25
 
-# another way to call in a file without importing os
-#csvFile = "~/Documents/WashU_Bootcamp/Homework/budget_data.csv"
+# another way to call in a file without importing os "but then your team and TAs will hate you" -JM he he
+# csvFile = "~/Documents/WashU_Bootcamp/Homework/budget_data.csv"
+
+# Reading the file and for each row of data, adding it to the csvDataset list variables defined above
 with open(csvFile,'r') as bankFile:
         csvRead = csv.reader(bankFile,delimiter=',')
-        print(csvRead)
-        csv_header = next(csvRead)                      #skips the header
-        print(f"CSV Header: {csv_header}")
+        #print(csvRead)                                 # prints the bankFile
+        csv_header = next(csvRead)                      # skips the header
+        #print(f"CSV Header: {csv_header}")
         for row in csvRead:
-            print(row)
-            csvDataset.append(row)
-            csvDataset_Month.append(row[0])
-            csvDataset_Amount.append(int(row[1]))
+            #print(row)
+            csvDataset.append(row)                      # adding the row contents to the list
+            csvDataset_Month.append(row[0])             # adding the first item (date) to a separate list using the index 0 (zero)
+            csvDataset_Amount.append(int(row[1]))       # adding the second item (amount) to a separate list using the index 1 (one)
+# I can see using a program to create my grocery list - 
+# except I really like handwriting that list - threw this random thought in
+# to make sure you're reading all these lovely comments ;P
 
-#The total number of months included in the dataset
+# The total number of months included in the dataset using the length function
 total_months = len(csvDataset_Month)
 
-#The total net amount of "Profit/Losses" over the entire period
-net_total = 0
-for month, amount in csvDataset:
-    net_total += int(amount)
+# The total net amount of "Profit/Losses" over the entire period
+net_total = 0                                           # setting the initial variable amount to zero
+for month, amount in csvDataset:                        # for the month and the amount in the dataset
+    net_total += int(amount)                            # adding each amount to the net total variable
 
-#The average change in "Profit/Losses" between months over the entire period
-chgAmt = 0
-monthChg = []
-for amount in range(1,len(csvDataset_Amount)):
-    # subtract the amount above from the amount below in the data set
+# The average change in "Profit/Losses" between months over the entire period
+chgAmt = 0                                              # setting the initial variable to zero
+monthChg = []                                           # creating ANOTHER list
+for amount in range(1,len(csvDataset_Amount)):          # for each amount from row 1 to the end
+    # Subtract the amount above from the amount below in the data set
     monthChg.append(csvDataset_Amount[amount] - csvDataset_Amount[amount-1])
 
-chgAmt = round(sum(monthChg) / len(monthChg),2)
-#print(str(chgAmt))
+chgAmt = round(sum(monthChg) / len(monthChg),2)         # calculating the average change over the entire period
+# print(str(chgAmt))
 
-#The greatest increase in profits (date and amount) over the entire period
-#The greatest loss (date and amount) over the entire period
-for amount in range(1,len(csvDataset_Amount)):
-    maxIncr = max(monthChg)
-    maxIncrIndex = monthChg.index(maxIncr)
-    maxDecr = min(monthChg)
-    maxDecrIndex = monthChg.index(maxDecr) 
+# The greatest increase in profits (date and amount) over the entire period
+# The greatest loss (date and amount) over the entire period
+for amount in range(1,len(csvDataset_Amount)):          # for each amount from row 1 to the end
+    maxIncr = max(monthChg)                             # getting the greatest increase using max function
+    maxIncrIndex = monthChg.index(maxIncr)              # I did not yet understand the beauty of dictionaries
+    maxDecr = min(monthChg)                             # getting the greatest decrease using min function
+    maxDecrIndex = monthChg.index(maxDecr)              # had to get creative because I did not want to understand dictionaries yet
 
-monthMaxIncr = csvDataset_Month[maxIncrIndex + 1]
-monthMaxDecr = csvDataset_Month[maxDecrIndex + 1]
+monthMaxIncr = csvDataset_Month[maxIncrIndex + 1]       # incrementing the index by 1 (one) since the first value in this list started with the second row
+monthMaxDecr = csvDataset_Month[maxDecrIndex + 1]       # same - incrementing the index
 
-#The final print out
+# The final print out - Whoot!
 print("Financial Analysis")
 print(f"{lines}") 
 print(f"Total Months: {total_months}")
@@ -71,6 +72,14 @@ print(f"Average Change: ${chgAmt}")
 print(f"Greatest Increase in Profits: {monthMaxIncr} (${maxIncr})")
 print(f"Greatest Decrease in Profits: {monthMaxDecr} (${maxDecr})")
 
-# export results to text file
-output_file = os.path.join("pyBank.csv")
-
+#  Write to a text file - got this from my friend, Learn Python the Hard Way / cross your fingers and pray this works!
+target = open("pyBank.txt", 'w')
+target.write("Financial Analysis\n")
+target.write(f"{lines}\n") 
+target.write(f"Total Months: {total_months}\n")
+target.write(f"Total: ${net_total}\n")
+target.write(f"Average Change: ${chgAmt}\n")
+target.write(f"Greatest Increase in Profits: {monthMaxIncr} (${maxIncr})\n")
+target.write(f"Greatest Decrease in Profits: {monthMaxDecr} (${maxDecr})\n")
+# Yay, it works! As a CPA, I find the formatting to be horrendous, someone give me a comma for those numbers >= $1,000.00
+# This was fun
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,10 +1,6 @@
-#Read the csv file
+# Read the csv file
 import os
-import csv 
-
-# Path to collect data from the Resources folder
-# need to adjust this because the file is not 
-# sitting in the PyBank folder
+import csv
 
 # getting the csv file (data set)
 csvFile = os.path.join('election_data.csv')
@@ -20,67 +16,91 @@ summary_votes = []
 # creating formatting for print
 lines = "-" * 25
 
-# another way to call in a file without importing os
-#csvFile = "~/Documents/WashU_Bootcamp/Homework/election_data.csv"
+# another way to call in a file without importing os is using pandas - holy cow, that is so much simpler
 with open(csvFile,'r') as electionFile:
         csvRead = csv.reader(electionFile,delimiter=',')
-        print(csvRead)
+        #print(csvRead)
         csv_header = next(csvRead)                      #skips the header
-        print(f"CSV Header: {csv_header}")
+        #print(f"CSV Header: {csv_header}")
         for row in csvRead:
             # print(row)
             # this creates a list of dictionaries (i.e. one dictionary for each voter ID)
+            # you missed a great white boarding session as I figured this out
+            # adds the keys for the header and selects each value by its index
             csvDataset.append({
             'voterID': row[0], \
             'county': row[1], \
             'candidate': row[2]
             })
-            csvDataset_Candidate.append(row[2])
-            csvDataset_VoterID.append(row[0])
-            
+            csvDataset_Candidate.append(row[2])             # giving myself list options for later
+            csvDataset_VoterID.append(row[0])               # same
 
 # the total number of votes cast
 total_votes = len(csvDataset_VoterID)
 # a complete list of candidates who received votes
 unique_candidate = []
-def unique(csvDataset_Candidate):
-    # global allows write access to the variable unique candidate
+def unique():
+    # global allows write access to the list variable, unique_candidate
     global unique_candidate     
     #loop through all elements in list
     for x in csvDataset_Candidate:
         # check if candidate exists in the unique list or not
         if x not in unique_candidate:
             unique_candidate.append(x)
-    for x in unique_candidate:
-        print(x)
+    #for x in unique_candidate:
+        #print(x)
 
-# calling the function 
-unique(csvDataset_Candidate)
+# calling the function because if you don't then nothing happens
+unique()
 
 # the percentage of votes each candidate won
 # the total number of votes each candidate won
 def sumVotes():
     for z in unique_candidate:                      # z becomes the candidate name  
         votes = 0                                   # variable resets to zero each loop   
-        print(z)   
+        #print(z)   
         for c in csvDataset:                        # c is the counter that loops through the dictionary
-            if z == c["candidate"]:                 # because z always equals c (duh)
+            if z == c["candidate"]:                 # because z always equals c (duh) - see vba homework
                 votes += 1                          # tally of votes per candidate
-        print(votes)
-        pctVotes = round((votes/total_votes), 2)
+        #print(votes)
+        pctVotes = round(((votes/total_votes) * 100), 3)     # calculating the percentage of votes with three decimals
+        # adding each key and value to a dictionary
         summary_votes.append({'candidate': z,'pctVotes': pctVotes, 'totVotes': votes})
 
-sumVotes()
-print(summary_votes)
+sumVotes()                                          # calling the function
+#print(summary_votes)                                
 
 # the winner of the election based on popular vote 
-# TBD
+winnerChickenDinner = None                          # setting variable to no one
+def winner():
+    rockTheVote = 0                                 # setting variable to zero
+    global winnerChickenDinner                      # made this global b/c it wouldn't print otherwise
+    for x in summary_votes:                         # looping through total votes in the dictionary
+        if x['totVotes'] > rockTheVote:             # if the value is the maximum then that is the winner
+            rockTheVote = x['totVotes']             # getting and storing max votes
+            winnerChickenDinner = x['candidate']    # the name of the winner if they have the max votes
 
-print("Election Results")
+winner()
+
+print("Election Results\n")
+print(f"{lines}\n")
+print(f"Total Votes: {total_votes}\n")
+print(f"{lines}\n")
+for x in summary_votes:
+    print("{0}: {1:.3f}% ({2:.0f})\n".format(x['candidate'], x['pctVotes'], x['totVotes']))
+print(f"{lines}\n")
+print(f"Winner: {winnerChickenDinner}\n")
 print(f"{lines}")
-print("Total Votes: {total_votes}")
-print(f"{lines}")
-print(f"{unique_candidate[0]}")
-print(f"{unique_candidate[1]}")
-print(f"{unique_candidate[2]}")
-print(f"{unique_candidate[3]}")
+
+#  Write to a text file - got this from my friend, Learn Python the Hard Way / cross your fingers and pray this works!
+target = open("pyPoll.txt", 'w')
+
+target.write("Election Results\n")
+target.write(f"{lines}\n")
+target.write(f"Total Votes: {total_votes}\n")
+target.write(f"{lines}\n")
+for x in summary_votes:
+    target.write("{0}: {1:.3f}% ({2:.0f})\n".format(x['candidate'], x['pctVotes'], x['totVotes']))
+target.write(f"{lines}\n")
+target.write(f"Winner: {winnerChickenDinner}\n")
+target.write(f"{lines}")
